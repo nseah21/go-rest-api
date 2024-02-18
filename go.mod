3b91fc11537df0ab64217f04ec065c207e2cca37
nseah21 <nseah21@gmail.com>
2024-02-18 15:21:49 +0800

    Configure database connection from server

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module example.com/go-rest-api
 
 go 1.21.6
+
+require github.com/lib/pq v1.10.9
+
+require github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e
--- internal/server/app.go
+++ internal/server/app.go
@@ -2,8 +2,39 @@ package server
 
 import (
 	"log"
+	"fmt"
+	"database/sql"
+	"os"
+	"github.com/lpernett/godotenv"
+	
+	_ "github.com/lib/pq"
 )
 
 func Start() {
 	log.Println("Starting server...")
+}
+
+func getDB() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+	host      := os.Getenv("DB_HOST")
+	port      := os.Getenv("DB_PORT")
+	user      := os.Getenv("DB_USER")
+	password  := os.Getenv("DB_PASSWORD")
+	dbname    := os.Getenv("DB_NAME")
+
+	formatStr := "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable"
+	connStr := fmt.Sprintf(formatStr, host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
